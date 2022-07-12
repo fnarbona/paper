d3d4ac4c59d5d67efaee87dc9ace61cdf9cff28d
Fernando Narbona <fnarbona@outlook.com>
2022-07-12 00:54:36 -0400

    adding basic setup for cypress

--- jest.config.js
+++ jest.config.js
@@ -13,7 +13,7 @@ const customJestConfig = {
 	// if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
 	moduleDirectories: ['node_modules', '<rootDir>/'],
 	testEnvironment: 'jest-environment-jsdom',
-	// modulePathIgnorePatterns: ['<rootDir>/lib/*'],
+	modulePathIgnorePatterns: ['<rootDir>/lib/*', '<rootDir>/cypress/*'],
 	// testPathIgnorePatterns: ['<rootDir>/lib/*'],
 	coveragePathIgnorePatterns: ['<rootDir>/lib/*'],
 	// coverageThreshold: {
